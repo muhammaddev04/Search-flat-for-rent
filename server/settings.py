cda55d7a2c6feaa049d6e8acb01180340470d89c
muhammaddev04 <muhammadmahmudzoda04@gmail.com>
2026-06-26 15:00:09 +0500

    Add Django project with dark and light function

--- server/settings.py
+++ server/settings.py
@@ -139,6 +139,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'muhammadmahmudzoda04@gmail.com'
-EMAIL_HOST_PASSWORD = 'vbgv rdky gfkv hulr'
+EMAIL_HOST_PASSWORD = 'uzqi wwvh xmre tcls'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
